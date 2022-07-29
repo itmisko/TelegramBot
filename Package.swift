4a19b78d0bc9a1d1a4f3794420e4b3a0177b34b1
Sergei Mishchenko <sergei@itmisko.misko>
2022-07-29 19:51:49 +0300

    TelegramBot

--- Package.swift
+++ Package.swift
@@ -8,13 +8,15 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.45.0")),
+        .package(url: "https://github.com/nerzh/telegram-vapor-bot", .upToNextMajor(from: "1.2.4")),
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "telegram-vapor-bot", package: "telegram-vapor-bot"),
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,9 +1,24 @@
 import Vapor
+import telegram_vapor_bot
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    app.http.server.configuration.hostname = "0.0.0.0"
+    
+    app.http.server.configuration.port = 80
+
+    let tgApi: String = "5447549874:AAE80shO5DhupGukLHwLP-drcfl4XyHLIUo"
+    let connection: TGConnectionPrtcl = TGLongPollingConnection()
+    TGBot.configure(connection: connection, botId: tgApi, vaporClient: app.client)
+    try TGBot.shared.start()
+
+    /// set level of debug if you needed
+    TGBot.log.logLevel = .error
+
+    DefaultBotHandlers.addHandlers(app: app, bot: TGBot.shared)
 
     // register routes
     try routes(app)
